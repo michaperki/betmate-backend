8cb506a033a3dfd0a6a82a73829cf09232898909
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-04-15 15:21:26 -0600

    move microservice types into types directory

--- src/services/microservice.ts
+++ src/services/microservice.ts
@@ -1,18 +1,7 @@
 import axios from 'axios';
 import querystring from 'querystring';
+import { WDLData, WDLResponse } from 'types/microservice';
 import { MICROSERVICE_URL } from '../helpers/constants';
-
-type WDLData = {
-  white_win: number,
-  draw: number,
-  black_win: number
-};
-
-type WDLResponse = {
-  message: string,
-  data: WDLData | null,
-  error: string | null
-};
 
 const getWDL = (fen: string, white_time: number, black_time: number): Promise<WDLData | null> => axios
   .get<WDLResponse>(`${MICROSERVICE_URL}/models/wdl?${querystring.stringify({ fen, white_time, black_time })}`)
