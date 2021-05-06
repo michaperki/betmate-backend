27da5709572ba8182de286653935abd27f442c33
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-05-05 23:08:09 -0600

    feat: sync websocket message schemas with frontend

--- src/websockets/chess_websocket.ts
+++ src/websockets/chess_websocket.ts
@@ -14,29 +14,42 @@ const websocket = (socket: Socket): void => {
 
   socket.on('join_game', async (gameId: string) => {
     const chessDoc = await chessController.getChessGame(gameId);
+    console.log('got request for', gameId);
     if (!chessDoc) return socket.emit('error', { gameId, message: 'Could not find game' });
 
     socket.join(gameId);
-    // console.log(`joined ${gameId}`);
-    // get game state
+    console.log(`joined ${gameId}`);
 
     return socket.emit('game_info', { gameId, data: chessDoc.toJSON() });
+  });
+
+  socket.on('leave_game', (gameId: string) => {
+    socket.leave(gameId);
+    console.log('left game', gameId);
+    return socket.emit('leave_game', { gameId, message: 'Left room' });
   });
 
   socket.on('new_move', async (move: { gameId: string, data: MoveData }): Promise<boolean> => {
     // need to add protection for who can move.
     const chessDoc = await chessController.getChessGame(move.gameId);
-    if (!chessDoc) return socket.emit('error', { gameId: move.gameId, message: 'Could not find game' });
+    if (!chessDoc) return socket.emit('game_error', { gameId: move.gameId, message: 'Could not find game' });
 
     const chessGame = new ChessGame(chessDoc.state);
 
     const moveResult = chessGame.move(move.data.san);
-    if (!moveResult) return socket.emit('error', { gameId: move.gameId, message: 'Invalid move' });
-
-    socket.to(move.gameId).emit('new_move', { gameId: move.gameId, data: move.data });
+    if (!moveResult) return socket.emit('game_error', { gameId: move.gameId, message: 'Invalid move' });
 
     const timeWhite = move.data.is_white ? move.data.time : chessDoc.time_white;
     const timeBlack = !move.data.is_white ? move.data.time : chessDoc.time_black;
+
+    const updateMessage = {
+      state: chessGame.fen(),
+      move_hist: chessGame.history(),
+      time_white: timeWhite,
+      time_black: timeBlack,
+    };
+
+    socket.to(move.gameId).emit('new_move', { gameId: move.gameId, ...updateMessage });
 
     const wdlOdds = await microservice
       .getWDL(chessGame.fen(), timeWhite, timeBlack)
@@ -47,7 +60,7 @@ const websocket = (socket: Socket): void => {
     // resolve wagers on the move just played, if any
     resolveCriticalMoveBets(move.gameId, chessGame).then((wagerResults) => {
       if (wagerResults) socket.to(move.gameId).emit('wager_result', { gameId: move.gameId, data: wagerResults.map((w) => w.toJSON()) });
-      else socket.to(move.gameId).emit('error', { gameId: move.gameId, message: 'There was an error updating critical move wagers' });
+      else socket.to(move.gameId).emit('game_error', { gameId: move.gameId, message: 'There was an error updating critical move wagers' });
     });
 
     const gameStatus = getChessStatus(chessGame);
@@ -65,13 +78,20 @@ const websocket = (socket: Socket): void => {
     };
 
     const result = await chessController.updateChessGame(move.gameId, fields);
-    if (!result) socket.to(move.gameId).emit('error', { gameId: move.gameId, message: 'There was an error saving' });
+    if (!result) socket.to(move.gameId).emit('game_error', { gameId: move.gameId, message: 'There was an error saving' });
 
     // update wagers for each user
     if (complete) {
+      const completeFields: UpdateQuery<ChessDoc> = {
+        complete: true,
+        game_status: gameStatus,
+      };
+      socket.to(move.gameId).emit('game_over', { gameId: move.gameId, ...completeFields });
+      await chessController.updateChessGame(move.gameId, completeFields);
+
       resolveWdlBets(move.gameId, gameStatus).then((wagerResults) => {
         if (wagerResults) socket.to(move.gameId).emit('wager_result', { gameId: move.gameId, data: wagerResults.map((w) => w.toJSON()) });
-        else socket.to(move.gameId).emit('error', { gameId: move.gameId, message: 'There was an error updating critical move wagers' });
+        else socket.to(move.gameId).emit('game_error', { gameId: move.gameId, message: 'There was an error updating critical move wagers' });
       });
     }
     return true;
