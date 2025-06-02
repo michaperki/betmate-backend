b024d0a9cc7a712e3d724ac408bd5ac21a5b86a2
Michael Perkins <mperkins1995@gmail.com>
2025-06-02 13:34:15 -0500

    fix: sync TypeScript fixes from release branch and add placeholder for twitter_service

---
+++ src/services/twitter_service.ts
@@ -0,0 +1,12 @@
+// Twitter service placeholder
+// This service will handle Twitter integration
+
+/**
+ * Twitter service for social media integration
+ * Note: Implementation will be added in a future update
+ */
+const twitterService = {
+  // Placeholder for future Twitter integration
+};
+
+export default twitterService;
