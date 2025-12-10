b10581924eaa4e747fa30e3d5130cbb012356a54
Michael Perkins <mperkins1995@gmail.com>
2025-12-10 10:58:50 +0200

    fix(auth): export requireAdminKey in authentication index

--- src/authentication/index.ts
+++ src/authentication/index.ts
@@ -1,5 +1,6 @@
 import requireAuth from './requireAuth';
 import requireSignin from './requireSignin';
 import { requireBotAuth } from './requireBotAuth';
+import { requireAdminKey } from './requireAdminKey';
 
-export { requireAuth, requireSignin, requireBotAuth };
+export { requireAuth, requireSignin, requireBotAuth, requireAdminKey };
